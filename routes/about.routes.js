6a551d5b51d706ace709d10d6ef450d778a62154
JesusSantana <jesus@esmarketingdigital.com>
2020-06-16 12:57:26 +0200

    Routes Second Commit

--- routes/about.routes.js
+++ routes/about.routes.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET About */
-router.get('/about', (req, res, next) => {
-  res.render('about');
+router.get("/about", (req, res, next) => {
+  res.render("about");
 });
 
 module.exports = router;
--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 /* GET Wallet */
 router.get("/app/wallet", (req, res, next) => {
   if (req.session.user) {
-    res.render("app/wallet" , req.session.user);
+    res.render("app/wallet", req.session.user);
   } else res.render("auth/login");
 });
 
