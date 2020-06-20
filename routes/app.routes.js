53f00cee9aee098940468e3c579ba31e837ea6d8
Marc De Mena <mdemena@gmail.com>
2020-06-20 09:23:24 +0200

    Firsts changes

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -4,7 +4,11 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => {
 	console.log('We arrive!!');
-	res.render('app/index', { layout: 'app/layout' });
+	res.render('app/index', {
+		layout: 'app/layout',
+		user: req.session.user,
+		wallet: req.session.wallet,
+	});
 });
 
 module.exports = router;
