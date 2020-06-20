d4fb92003d99b3e76a5f3af8ea32fee150d63493
JesusSantana <jesus@esmarketingdigital.com>
2020-06-20 15:04:27 +0200

    Profile

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-	console.log('We arrive!!');
-	res.render('app/index', { layout: 'app/layout' });
+    console.log('We arrive!!');
+    res.render('app/index', {
+        layout: 'app/layout',
+        user: req.session.user,
+        wallet: req.session.wallet,
+    });
 });
 
 module.exports = router;
