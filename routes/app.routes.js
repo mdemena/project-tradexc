348ce1030dc14e566fbbad5159fb6866cdc012f9
Marc De Mena <mdemena@gmail.com>
2020-06-23 19:21:55 +0200

    Urgent Fix

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -7,13 +7,15 @@ const tradeController = require('../controllers/trade.controller');
 /* GET home page */
 router.get('/', async (req, res, next) => {
 	const support = await supportController.listByUser(req.session.user._id);
-	const transactions = await transactionsController.listByUser(req.session.user_id);
+	const transactions = await transactionsController.listByUser(
+		req.session.user_id
+	);
 
-  // Begin Dashboard data
+	// Begin Dashboard data
 	let transAmount = 0;
 	let transCount = 0;
-	let transBuys = 0;
-	let transSells = 0;
+	let transBuysCount = 0;
+	let transSellsCount = 0;
 	let balanceBuySell = 0;
 	let walletAmount = req.session.wallet.amount;
 	const balanceInvest = await tradeController.groupedByUserBySymbol(
@@ -21,13 +23,13 @@ router.get('/', async (req, res, next) => {
 	);
 
 	if (transactions.length > 0) {
-		transBuys = transactions.filter((trans) => trans.type === 'buy');
-		transSells = transactions.filter((trans) => trans.type === 'sell');
-		transBuysAmount = transBuys.reduce(
+		const transBuys = transactions.filter((trans) => trans.type === 'buy');
+		const transSells = transactions.filter((trans) => trans.type === 'sell');
+		const transBuysAmount = transBuys.reduce(
 			(total, trans) => (total += trans.total),
 			0
 		);
-		transSellsAmount = transSells.reduce(
+		const transSellsAmount = transSells.reduce(
 			(total, trans) => (total += trans.total),
 			0
 		);
@@ -51,7 +53,6 @@ router.get('/', async (req, res, next) => {
 		balanceBuySell: balanceBuySell * 100,
 		balanceInvest: balanceInvest,
 		supports: support,
-		
 	});
 });
 
